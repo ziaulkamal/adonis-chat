09582d8b6de504512426f6c97dffcb301d46e52d
Ziaul Kamal <ziaulkamal1109@gmail.com>
2023-03-12 12:44:17 +0700

    Template Setup

--- app/Controllers/Http/LogonsController.ts
+++ app/Controllers/Http/LogonsController.ts
@@ -1,3 +1,20 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class LogonsController {}
+export default class LogonsController {
+    public index({ view }: HttpContextContract) {
+        return view.render('login')
+    }
+
+    public async login({ auth, request, response }: HttpContextContract) {
+        const username = request.input('username')
+        const room = request.input('room')
+
+        try {
+            await auth.use('web').attempt(username, room)
+            return response.redirect().toRoute('chats.index')
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,5 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+Route.get('/', 'LogonsController.index')
+Route.post('/', 'LogonsController.login').as('login')
